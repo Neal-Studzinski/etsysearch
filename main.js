35b7ded9939152ece82668155ac60855a0aa3d75
NEAL STUDZINSKI <nealstudzinski@gmail.com>
2017-03-21 07:45:30 -0500

    not working

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 const API_KEY = 's0xg1ymmj1foh3jx1i2ggbyf';
-var contentDisplay = $('.container');
-var formElement = $('form')
+var contentDisplay = $('.search-images');
+var formElement = $('.search-form')
+var searchQuery;
 
 $(formElement).on('submit', function(e) {
   e.preventDefault()
-  var searchQuery = $('input').val();
-  $(contentDisplay).text(searchQuery);
-});
+  var searchQuery = $('.search-query').val();
+
+  ;
+
+var settings =    {
+        type: 'GET',
+        url: 'https://api.etsy.com/v2/listings/active.js?api_key=' + API_KEY + '&keywords=' + searchQuery + '&includes=Images,Shop',
+        dataType: 'jsonp',
+        success: function(data, status, xhr){
+          var results = data.results;
+          results.forEach(function(item, i, arr){
+            var images = item.Images[0].url_170x135;
+            var shop = item.Shop.shop_name;
+
+            var test = $('<div class="card"><div class="image-container"><img src="'+ images +'"></div><p class="title">'+item.title+'</p><p><span class="shop">'+ shop +'</span><span class="price">$'+item.price+'</span></p></div>');
+            $('.contentDisplay').append(test);
+  })
+}
+};
+
+
+//jQueryElement.on('nameOfEvent', function(e) {
+
+//});
+
+
+
+//$jQueryElement.attr('nameOfAttr', 'someNewValue');
